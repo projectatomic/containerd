f9a2eeb64054e740fb1ae3048dde153c257113c8
Ulrich Obergfell <uobergfe@redhat.com>
2019-10-10 11:16:50 +0200

    revert changes introduced by "fix error handling in restore() function", commit e96a10aef66e (https://github.com/projectatomic/containerd/pull/10)
    Signed-off-by: Ulrich Obergfell <uobergfe@redhat.com>

--- runtime/container.go
+++ runtime/container.go
@@ -146,15 +146,15 @@ func New(opts ContainerOpts) (Container, error) {
 }
 
 // Load return a new container from the matchin state file on disk.
-func Load(root, id, shimName string, timeout time.Duration) (Container, error, string) {
+func Load(root, id, shimName string, timeout time.Duration) (Container, error) {
 	var s state
 	f, err := os.Open(filepath.Join(root, id, StateFile))
 	if err != nil {
-		return nil, err, "init"
+		return nil, err
 	}
 	defer f.Close()
 	if err := json.NewDecoder(f).Decode(&s); err != nil {
-		return nil, err, "init"
+		return nil, err
 	}
 	c := &container{
 		root:        root,
@@ -175,7 +175,7 @@ func Load(root, id, shimName string, timeout time.Duration) (Container, error, s
 
 	dirs, err := ioutil.ReadDir(filepath.Join(root, id))
 	if err != nil {
-		return nil, err, "init"
+		return nil, err
 	}
 	for _, d := range dirs {
 		if !d.IsDir() {
@@ -184,7 +184,7 @@ func Load(root, id, shimName string, timeout time.Duration) (Container, error, s
 		pid := d.Name()
 		s, err := readProcessState(filepath.Join(root, id, pid))
 		if err != nil {
-			return nil, err, pid
+			return nil, err
 		}
 		p, err := loadProcess(filepath.Join(root, id, pid), pid, c, s)
 		if err != nil {
@@ -193,7 +193,7 @@ func Load(root, id, shimName string, timeout time.Duration) (Container, error, s
 		}
 		c.processes[pid] = p
 	}
-	return c, nil, ""
+	return c, nil
 }
 
 func readProcessState(dir string) (*ProcessState, error) {
